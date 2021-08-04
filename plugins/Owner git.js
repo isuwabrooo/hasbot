4c41b6189d19163f0420857cc7b161fc6f55405e
Mikhaiel_Offical <81913716+Mikhaiel@users.noreply.github.com>
2021-08-04 18:21:24 +0530

    UPDATE 💝

--- plugins/Owner git.js
+++ plugins/Owner git.js
@@ -1,63 +1,29 @@
-const asena = require('../events');
-const {MessageType} = require('@adiwajshing/baileys');
-const OWNER = "it sends details of owner"
-const GIT = "it sends links"
-const Config = require('../config');
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
 
+const Language = require('../language');
+const Lang = Language.getString('wallpaper');
 
-if (Config.WORKTYPE == 'private') {
-        asena.addCommand({pattern: 'owner', fromMe: true, deleteCommand: true, desc: OWNER,}, (async (message, match) => {
-
-    var r_text = new Array ();
-    
-    r_text[1] = "*╔═════Jimbrootan🧞‍♂️═════╗*\n           \n *😈═Jimbrootan🧞‍♂️═😈* \n\n *owner Mikhaiel - http://Wa.me/+919544846609 \n* *\n🔰instagram:-https://www.instagram.com/the_real_mikhaiel* \n *╚══════♻️═════╝* \n\n *▷Creator: Mikhaiel*"
-
-    
-    await message.client.sendMessage(
-        message.jid,(r_text[1]), MessageType.text);
-
-    }));
-
-
-        asena.addCommand({pattern: 'git', fromMe: true, deleteCommand: true, desc: GIT,}, (async (message, match) => {
-
-        var r_text = new Array ();
-    
-        r_text[1] = "*Git links*\n           *\n💥═Jinn🧞‍♂️ Owner Mikhaiel═💥*\n\n*💘https://github.com/Mikhaiel/jinnh*\n*"
-
-    
-        await message.client.sendMessage(
-            message.jid,(r_text[1]), MessageType.text);
-    
-        }));    
-
-    }
-    
-
-    if (Config.WORKTYPE == 'public') {
-        asena.addCommand({pattern: 'owner', fromMe: false, deleteCommand: true, desc: OWNER,}, (async (message, match) => {
+Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message, match) => {
 
     var r_text = new Array ();
     
-    r_text[1] = "*╔═════Jimbrootan🧞‍♂️═════╗*\n                 \n*⚜═Jinn🧞‍♂️═⚜*\n\n*owner Mikhaiel - http://Wa.me/+919544846609*\n *\n🔰instagram:-https://www.instagram.com/the_real_Mikhaiel*            *\n*╚══════♻️═════╝*\n\n* ▷Creator: Mikhaiel*"
-
     
-    await message.client.sendMessage(
-        message.jid,(r_text[1]), MessageType.text);
-
-    }));
-
-
-        asena.addCommand({pattern: 'git', fromMe: false, deleteCommand: true, desc: GIT,}, (async (message, match) => {
-
-        var r_text = new Array ();
+   
+  r_text[0] = "https://c4.wallpaperflare.com/wallpaper/195/797/214/aladdin-aladdin-gin-hd-wallpaper-preview.jpg";
     
-        r_text[1] = "*Git links*\n           *\n💥═Jinn🧞‍♂️ owner Mikhaiel═💥 \n\n 💘https://github.com/Mikhaiel/jinnh \n*"
-
     
-        await message.client.sendMessage(
-            message.jid,(r_text[1]), MessageType.text);
-    
-        }));    
+    var i = Math.floor(1*Math.random())
 
-    }
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Creater Mikhaiel*
+*Bgm updation💘*
+*Owner number wa.me/919544846609*
+*githublink       https://github.com/Mikhaiel/Jinnh*
+*audio commads    https://github.com/Mikhaiel/Jinnh/tree/master/uploads*
+*sticker commads  https://github.com/Mikhaiel/Jinnh/tree/master/stickers*
+`}) 
+
+}));
