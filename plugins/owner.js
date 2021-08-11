df7c39ef8936bacbfde3038977e48e4b3d8a5e0b
Ajmal <88565942+Ajmal-Pro@users.noreply.github.com>
2021-08-11 19:06:02 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -18,7 +18,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Creater Achu*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {Nandhutty: Nandhutty.png, caption: `*Creater Achu*
 
 
 *Owner number Chat me with this bot number itself*
